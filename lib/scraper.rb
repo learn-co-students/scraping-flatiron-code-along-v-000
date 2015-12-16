cc4b8a09bae985aea27fafaa5fe2887ed879dabe
authorbeard <msnolan@gmail.com>
2015-12-15 20:35:50 -0500

    course spec passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,19 @@ require_relative './course.rb'
 
 class Scraper
   
+  def self.get_page
+    page = Nokogiri::HTML(open(http://learn-co-curriculum.github.io/site-for-scraping/courses))
+  end
+
+  def self.get_courses
+    courses = page.css("article")
+  end
+
+  def self.make_courses
+  end
+
+
+
 end
 
 
