6ea21de34c1f376571133979eb88b1608e10d890
acolbert1 <acolbert1@gmail.com>
2016-01-05 22:23:10 -0800

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,22 @@
+class Course
 
+attr_accessor :title, :schedule, :description
+@@all = []
 
+def initialize
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def self.reset_all
+  @@all.clear
+end
+
+def get_courses
+  self.get_page.css(".post")
+end
+
+end
