276e164af3c5a1511d0b27727a8ae317d0772706
azheng249 <andy.zheng249@gmail.com>
2016-01-09 18:31:13 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
+  def self.all
+    @@all
+  end
+  def self.reset_all
+    @@all.clear
+  end
+
+  def initialize(title = nil, schedule = nil, description = nil)
+    @title = title
+    @schedule = schedule
+    @description = description
+    self.class.all << self
+  end
+end
 
