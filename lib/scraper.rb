0d3c539659911e0ba9147e194c7f58fa00183666
shmulim <shmuel.markel@gmail.com>
2016-01-24 12:38:26 -0800

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,7 @@ class Scraper
     end
 
     def make_courses
-      get_courses.each do |post|
+      self.get_courses.each do |post|
         course = Course.new
         course.title =  post.css('h2').text
         course.description = post.css('p').text
