7d5d8d64b11307d5e643c66ccab6d1a4851f9bfe
jmheyd <heydjm@comcast.net>
2016-02-09 13:16:52 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,19 +4,45 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
+
+    def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    end
+
+
+#call on .get_page method inside the .get_courses method
+    def get_courses
+      self.get_page.css(".post")
+    end
   
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
+
+#make_courses method should operate on the collection of course offering Nokogiri XML elements 
+#that was returned by the .get_courses method.
+#The make_courses method should iterate over the collection and make a new instance of Course
+#class for each one while assigning it the appropriate attributes.
+    def make_courses
+      get_courses.each do |post|
+       course = Course.new
+       course.title = post.css("h2").text
+       course.schedule = post.css(".date").text
+       course.description = post.css("p").text
       end
     end
-  end
+
+    def print_courses
+      self.make_courses
+      Course.all.each do |course|
+        if course.title
+          puts "Title: #{course.title}"
+          puts "  Schedule: #{course.schedule}"
+          puts "  Description: #{course.description}"
+        end
+      end
+    end
   
 end
 
+#Scraper.new.get_page
+Scraper.new.print_courses
 
 
