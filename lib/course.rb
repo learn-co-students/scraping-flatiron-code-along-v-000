7ddcbf436863cde082d71f263bc86ad465ca2dab
Valentin Rosu <alispace410@gmail.com>
2016-03-27 17:45:56 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@courses = []
+  def initialize
+    @@courses.push(self)
+  end
+
+  def self.all
+    @@courses
+  end
+
+  def self.reset_all
+    @@courses = []
+  end
+end
 
