b9ae55e76cc2a3ac75adeb4f6f32082973aca208
github username <github email address>
2016-04-05 13:09:02 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,13 +8,6 @@ class Scraper
 
     def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      
-      # doc.css(".post").each do |post|
-      #   course = Course.new
-      #   course.title = post.css("h2").text
-      #   course.schedule = post.css(".date").text
-      #   course.description = post.css("p").text
-      # end
     end
 
     def get_courses
@@ -44,7 +37,6 @@ class Scraper
   
 end
 
-Scraper.new.get_page
 
 Scraper.new.print_courses
 
