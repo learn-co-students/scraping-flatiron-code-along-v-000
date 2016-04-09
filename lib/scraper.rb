ed739629a85e9571ee4ab5a92180382713434fb9
Robert Koeze <rkoeze@gmail.com>
2016-04-09 13:19:47 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,20 +6,19 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
-    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
-    doc = Nokogiri::HTML(html)
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def get_courses
-    self.get_page.css("text")
+    self.get_page.css(".post")
   end
 
   def make_courses
-    self.get_courses.map do |post|
+    self.get_courses.each do |post|
       course = Course.new
-      course.title = post.css("h2")
-      course.schedule = post.css(".date")
-      course.desciption = post.css("p")
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
   end
 
@@ -34,5 +33,6 @@ class Scraper
     end
   end
 
-
 end
+
+Scraper.new.print_courses
