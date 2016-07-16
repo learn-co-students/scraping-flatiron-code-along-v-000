2fe0f4ac358447a0106acb8105d5d7e8bffbbba4
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-16 22:43:54 +0000

    initial commit

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+class Course
+  attr_accessor :title, :description, :schedule
 
+  @@all=[]
 
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # this is a class method because it is called directly on the class. We need this to manipulate the instances of the class
+  def self.reset_all #resetting the array that holds all the instances of the class
+      @@all.clear
+  end
+
+end
