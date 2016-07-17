897b032820feb88818e627b2a2e7be9586a552a3
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-17 01:41:35 +0000

    second commit

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,14 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def print_courses
     self.make_courses
+
     Course.all.each do |course|
       if course.title
         puts "Title: #{course.title}"
@@ -14,9 +16,25 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
+
   end
-  
+
+  def get_page
+    # The page itself:
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    # The collection of course offerings:
+    doc.css(".post").each do |post|
+      course = Course.new
+      # The title of an individual course offering:
+      course.title = post.css("h2").text
+      # The schedule of an individual course offering:
+      course.schedule = post.css(".date").text
+      # The description of an individual course offering:
+      course.description = post.css("p").text
+      binding.pry
+    end
+
+  end
+
 end
-
-
-
+Scraper.new.get_page
