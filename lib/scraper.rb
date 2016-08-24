cdfeae34b8d0ce3fb446da4ff1c44e0f1ff96fd6
Drew Pham <drewvpham@gmail.com>
2016-08-24 19:36:48 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,23 @@ end
        course.title = post.css("h2").text
        course.schedule = post.css(".date").text
        course.description = post.css("p").text
+
         end
+
         end
+
+        def print_courses
+   self.make_courses
+   Course.all.each do |course|
+     if course.title
+       puts "Title: #{course.title}"
+       puts "  Schedule: #{course.schedule}"
+       puts "  Description: #{course.description}"
+     end
+   end
+ end
+
 end
-Scraper.new.get_page
+
+
+Scraper.new.print_courses
