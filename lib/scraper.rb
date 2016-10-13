0bbbc612b7eb90e560b77a82e69fb01d19861d90
Chirag Dudhat <cdudhat@gmail.com>
2016-10-13 03:30:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,9 +13,10 @@ class Scraper
     #doc.css(".post").each do |post|
     #  course = Course.new
     #  course.title = post.css("h2").text
-    #  course.schedule = post.css("em").text
+    #  course.schedule = post.css(".date").text
     #  course.description = post.css("p").text
     #end
+    #binding.pry
   end
 
   def get_courses
@@ -31,7 +32,7 @@ class Scraper
     self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
-      course.schedule = post.css("em").text
+      course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
   end
