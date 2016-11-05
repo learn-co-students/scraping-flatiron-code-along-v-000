a96d0a033bb37c96e7e52df4ea4180fdf1ef855a
Chenxi Wu <wuchenxi2013@gmail.com>
2016-11-05 08:51:39 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,13 @@
-
-
+class Course
+  attr_accessor :title, :schedule, :description
+  @@courses=[]
+  def self.all
+    @@courses
+  end
+  def self.reset_all
+    @@courses=[]
+  end
+  def initialize
+    @@courses<<self
+  end
+end
