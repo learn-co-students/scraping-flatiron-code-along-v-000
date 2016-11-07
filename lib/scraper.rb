3aacffbe66e4baf140359f779082ae5f5747492b
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-11-07 21:37:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,17 +17,23 @@ attr_accessor :html, :doc
   end
 
   def get_page
-    @html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
-    @doc = Nokogiri::HTML(@html)
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def get_courses
-    @doc = Nokogiri::HTML(@html)
-    @doc.css(".post")
+    self.get_page.css(".post")
   end
 
   def make_courses
-    
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
 
 end
+
+
+Scraper.new.print_courses
