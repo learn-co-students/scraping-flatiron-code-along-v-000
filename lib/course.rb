d641d537ffe14adcd26ebb4ebf1fad50a37ea1ed
Tyson Philips <sangdencre@gmail.com>
2016-11-25 04:52:40 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,13 @@
-
-
+class Course
+  attr_accessor :title, :schedule, :description
+  @@course = []
+  def initialize
+  	@@course << self
+  end
+  def self.all
+    @@course
+  end
+  def self.reset_all
+  	@@course.clear
+  end
+end
