79b4bc096b5e4add0ae511eef57704b485a95062
dwyn <dwayne.harmon@gmail.com>
2016-12-24 16:55:27 -0500

    Done.

--- lib/lovebot.rb
+++ lib/lovebot.rb
@@ -1,12 +1,14 @@
-class Course
-  attr_accessor :title, :schedule, :description
-  @@all = []
+require_relative './course.rb'
+
+module Lovebot
   def initialize
     @@all << self
   end
+
   def self.all
     @@all
   end
+
   def self.reset_all
     @@all.clear
   end
