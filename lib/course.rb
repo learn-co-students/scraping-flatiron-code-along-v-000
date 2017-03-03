28dc4ad032e080c3ecd62330937aa79dc3b78c93
Elias Ventura <eliasventura23@gmail.com>
2017-03-03 22:03:36 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -2,17 +2,19 @@ class Course
 
   attr_accessor :title, :schedule, :description
 
-@@all = []
+  @@all= []
 
   def initialize
     @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def self.reset_all
     @@all.clear
   end
 
-  def self.all
-    @@all
-  end
+
 end
