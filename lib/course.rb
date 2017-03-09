6b047ebf7396a7de4df2fcc4f343dc6f2aa457dd
Trai Compton <tlcompton2546@outlook.com>
2017-03-09 22:07:37 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,25 @@
+class Course
+  #attributes
+  attr_accessor :title, :schedule, :description
+  @@all = Array.new
+
+  #initialize
+  def initialize
+    @@all << self
+  end
+
+  #class methods
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+  #instance method
 
 
+  #class methods
+
+
+end
