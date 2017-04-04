664f54bff89cfe4571f9da1e8160acbc15b9948c
Laamia Islam <laamia.islam@gmail.com>
2017-04-04 14:21:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,23 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+    def get_page
+      html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+      Nokogiri::HTML(html)
+    end
+
+    def get_courses
+      self.get_page.css("post same-height-left" "post same-height-right")
+    end
+
+    def make_courses
+      course_array = []
+      self.get_courses.each do |course|
+        course_array << course.css("h2").text
+      end
+      course_array
+    end
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +31,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
