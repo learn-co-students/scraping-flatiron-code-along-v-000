3c1268f7d24e6e812959b1895d10e928583ed646
irina <iafanasyevaa@gmail.com>
2017-04-23 00:56:22 +0300

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,7 +32,7 @@ class Scraper
   def print_courses
     self.make_courses
     Course.all.each do |course|
-      if course.title
+      if !course.title.empty? #instead #if course.title  
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
