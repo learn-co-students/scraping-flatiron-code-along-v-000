2249741a6f91cd79a88073a65c7b8a080b10b729
Amanda George <github email address>
2017-05-02 14:48:01 -0500

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,22 @@
+class Course
 
+  attr_accessor :title, :schedule, :description
+  @@all =[]
 
+  def initialize
+    @@all << self
+    @@title = title
+    @@schedule = schedule
+    @@description = description
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+end
