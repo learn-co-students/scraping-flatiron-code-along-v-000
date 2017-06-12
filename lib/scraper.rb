9d569d240a4a1a0ad3d001ec126c76720e5c55b1
Ninian Bond <gentile.bond@gmail.com>
2017-06-12 16:59:41 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,25 +28,35 @@ class Scraper
   end
   def get_courses
 
-   @schedules = self.get_page.css(".post .date")
-   @titles = self.get_page.css(".post h2")
-   @descriptions = self.get_page.css(".post p")
+  #  @schedules = self.get_page.css(".post .date")
+  #  @titles = self.get_page.css(".post h2")
+  #  @descriptions = self.get_page.css(".post p")
+
+  self.get_page.css(".post")
   end
 
   def make_courses
-    course = Course.new
-    get_courses
-    @titles.each do |title|
-      # binding.pry
-      course.title = title.text
+    # course = Course.new
+    # get_courses
+    # @titles.each do |title|
+    #   # binding.pry
+    #   course.title = title.text
+    # end
+    # @schedules.each do |schedule|
+    #   course.schedule = schedule.text
+    # end
+    # @descriptions.each do |description|
+    #   course.description = description.text
+    # end
+    # course
+
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
-    @schedules.each do |schedule|
-      course.schedule = schedule.text
-    end
-    @descriptions.each do |description|
-      course.description = description.text
-    end
-    course
   end
 
 end
+Scraper.new.print_courses
