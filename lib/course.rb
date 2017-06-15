2f15d06e04904cfdb4c650b68bc2c5f56700a2b0
Noah Summers <summers.noah@gmail.com>
2017-06-14 20:16:04 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def initialize
+    self.class.all << self
+  end
+
+end
 
