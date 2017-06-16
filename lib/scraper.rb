e366ef853ff7b0dba9efab3f92275f15873019c7
mikka <mikkanthrope@gmail.com>
2017-06-15 23:32:03 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,15 +24,15 @@ class Scraper
     end
 
     def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
+      self.make_courses
+      Course.all.each do |course|
+        if course.title
+          puts "Title: #{course.title}"
+          puts "  Schedule: #{course.schedule}"
+          puts "  Description: #{course.description}"
+        end
       end
     end
-  end
 
 end
 
