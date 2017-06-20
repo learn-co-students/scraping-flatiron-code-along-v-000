4ab49b34d8dac43b18555a8aa5e5a647160c6e0d
Aly Alidina <aly_alidina@hotmail.com>
2017-06-20 23:28:57 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+class Course
 
+  attr_accessor :title, :schedule, :description #has a setter and a getter method for title, schedule and description
 
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all #returns and array of all the instances of the Course class 
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+end
