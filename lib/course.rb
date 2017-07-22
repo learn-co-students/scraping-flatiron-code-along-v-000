659e58d0a5665b05a75d759d2801dbf9a137a9cf
Enoch2k2 <enoch2k2@hotmail.com>
2017-07-22 12:48:19 -0500

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,21 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
 
+  def initialize
+    self.save
+  end
+
+  def save
+    self.class.all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+end
