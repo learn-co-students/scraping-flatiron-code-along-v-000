3b431fbb6d50c724a033e261c21baa864914c282
Alexandra Wright <superbiscuit@gmail.com>
2017-08-27 23:36:40 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,23 @@
+class Course
 
+	attr_accessor :title, :description, :schedule
+
+	@@all = []
+
+
+	def initialize
+		@@all << self
+	end
+
+	def self.reset_all
+		self.all.clear
+	end
+
+	def self.all
+		@@all
+	end
+
+
+
+end
 
