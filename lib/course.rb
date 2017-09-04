eb4dfc57b9cb61c105199df0ac6a1a1c27cb58d7
Chris Rudolph <chrisjames.r@gmail.com>
2017-09-04 22:02:44 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,26 @@
+class Course
+  attr_accessor :title, :schedule, :description
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  # def
+  #   doc.css(".post").each do |post|
+  #     course = Course.new
+  #     course.title = post.css("h2").text
+  #     course.schedule = post.css(".date").text
+  #     course.description = post.css("p").text
+  # end
 
 
+end  # End of Class
