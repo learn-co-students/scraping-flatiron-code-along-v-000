574e950236ee0291db7c758761159dcdae91fe63
Deborah Seow <deborahseow9@gmail.com>
2017-09-17 04:27:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +16,25 @@ class Scraper
       end
     end
   end
-  
+
+    def get_page
+    # since  html = open(), we can consolidate the two methods into one line, replacing html in the parenthesis for the method
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    end
+
+
+
+    def get_courses
+      self.get_page.css(".post")
+    end
+
+    def make_courses
+      self.get_courses.each do |post|
+        course = Course.new
+        course.title = post.css("h2").text #post.css(".post").first.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text #doc.css(".post").first.css(".date").text
+      #doc.css(".post").first.css("p").text
+    end
+  end
 end
-
-
-
