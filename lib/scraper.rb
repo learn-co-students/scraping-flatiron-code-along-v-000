2ac75027286f40f79ca26cb6ab486ee2b0a324c9
Gustavo Villagrana <gusvilla303@gmail.com>
2017-09-18 16:11:06 +0000

    Add get_page and get_courses instance methods to Scraper.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
 
+require 'pry'
+
 require_relative './course.rb'
 
 class Scraper
+
+  def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(html)
+  end
+
+  def get_courses
+    get_page.css(".posts-holder .post")
+  end
+
   
-    def print_courses
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +28,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
