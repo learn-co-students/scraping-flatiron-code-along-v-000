a3a00c7ddc9a3efbb70d8b32ab1e7c51b44fab7f
Tony Distinti <adistinti@gmail.com>
2017-10-04 12:24:34 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,20 @@
 class Course
-end
 
+  attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+
+end
