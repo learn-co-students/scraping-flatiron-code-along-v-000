432b46a85e2ee483231170d3ae29a98a439ad625
github username <github email address>
2017-11-18 15:08:50 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,21 +22,18 @@ class Scraper
          course.description = post.css("p").text
         end
      end
+
+     def print_courses
+        self.make_courses
+        Course.all.each do |course|
+        if course.title
+          puts "Title: #{course.title}"
+          puts "  Schedule: #{course.schedule}"
+          puts "  Description: #{course.description}"
+        end
+      end
+     end
+     
 end
 
-
-
-  
-#     def print_courses
-#     self.make_courses
-#     Course.all.each do |course|
-#       if course.title
-#         puts "Title: #{course.title}"
-#         puts "  Schedule: #{course.schedule}"
-#         puts "  Description: #{course.description}"
-#       end
-#     end
-#   end
-  
-# end
-
+Scraper.new.print_courses
