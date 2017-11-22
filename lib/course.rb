9d47c28eff90502a7631b074deaf1fecad86bbcf
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2017-11-22 16:51:37 -0500

    Rspeclemented Course and Scraper classes

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,15 @@
+require_relative "../concerns/memorable.rb"
 
+class Course
+    extend Memorable::ClassMethods
+    include Memorable::InstanceMethods
+
+    attr_accessor :title, :schedule, :description
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+end
 
