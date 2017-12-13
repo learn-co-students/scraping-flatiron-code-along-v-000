09bc809c8522a5a116ae9055d7ed4060aaec6d50
Charity <Youngblood.charity@gmail.com>
2017-12-13 13:24:56 -0800

    created placeholders for instance and class methods

--- lib/course.rb
+++ lib/course.rb
@@ -1,12 +1,27 @@
 require 'pry'
 
 # create a class Course 
-
+class Course 
 # create instance methods for:
-
+  def initialize
+    
+  end 
   # - title 
+  def title
+    
+  end 
   # - schedule
+  def schedule
+    
+  end 
   # - description 
+  def description 
+    
+  end 
 
 # create class method - self.all
+  def self.all
+    
+  end 
 
+end 
