48ee347af557a9a59a0a5228b6164491312a2047
Charity <Youngblood.charity@gmail.com>
2017-12-13 13:49:24 -0800

    replaced instance variable for courses with class variable all - added to methods

--- lib/course.rb
+++ lib/course.rb
@@ -5,20 +5,21 @@ class Course
 # create attr_accessor for title 
   attr_accessor :title, :schedule, :description
 # create instance methods for:
+  @@all = []
+  
   def initialize
-    
+    @@all << self
   end 
   
 # create class method - self.all
   def self.all
     # this method returns an array of all instances of Course class 
-    @courses = []
-    @courses << self 
+   @@all
    
   end 
   
   def self.reset_all 
-    @courses = []
+    @@all = []
   end 
 
 end 
