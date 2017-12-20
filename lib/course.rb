7d1c00226ebea5256f606446b26bfee3b345ee70
Yina Zhu <yinagit@gmail.com>
2017-12-20 05:14:10 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,16 @@
+class Course
 
+attr_accessor :title, :schedule, :description
+@@all = []
 
+def initialize
+  @@all << self
+end
+
+class self.all
+@@all
+end
+
+class self.reset_all
+  @@all.clear
+end
