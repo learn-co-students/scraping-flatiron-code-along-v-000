355e8e8cfdef83685d0d817c4688703f2c913379
Shangni Hu <shangnihu@gmail.com>
2017-12-26 05:13:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,15 @@ class Scraper
     self.get_page.css(".post")
   end
 
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
