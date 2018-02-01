897ff1c8d43f4e364913c0a9be2bf6c30ee6a96f
EPIC448 <samrey2018@gmail.com>
2018-02-01 19:06:40 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
+require 'pry'
+ class Course
+   attr_accessor :title, :description, :schedule
+   @@all = []  #keeps track of all the instances of the course.
+    def initialize
+      @@all << self
+    end
 
+    def self.all
+      # binding.pry
+      @@all
+    end
 
+    def self.reset_all
+      @@all.clear
+    end
+ end
