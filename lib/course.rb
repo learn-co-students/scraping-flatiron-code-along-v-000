507553e175131a047684cc94d70baef2a6e54c7a
Evan Candler <emcandler21@gmail.com>
2018-03-09 20:41:23 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
 
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
+
+  def initialize #that every time an instance of the class is initialized, we should push it into the @@all class variable.
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-require_relative './course.rb'
+require_relative './course.rb'#We are requiring our Course class file so that our Scraper can make new courses and give them attributes scraped from the web page.
+
+
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +18,33 @@ class Scraper
       end
     end
   end
-  
+
+def get_page
+Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+#  doc.css(".post").each do |post|
+#        course = Course.new
+#        course.title = post.css("h2").text
+#        course.schedule = post.css(".date").text
+#        course.description = post.css("p").text
+#      end
+#For each iteration over the collection of Nokogiri XML elements returned to us by the doc.css(".post") line, we are making a new instance of the Course class and giving that instance the title, schedule and description extracted from the XML.
+end
+
+def get_courses
+  self.get_page.css(".post") #returns the collection of Nokogiri XML elements
+end
+
+def make_courses
+  self.get_courses.each do |post|
+    course = Course.new
+ course.title = post.css("h2").text
+ course.schedule = post.css(".date").text
+ course.description = post.css("p").text
+  end
+end
+
 end
 
 
-
+Scraper.new.print_courses
