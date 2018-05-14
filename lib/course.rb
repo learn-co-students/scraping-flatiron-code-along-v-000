8164bd95e57661d38636f13782833b61680073c0
Paul <pcherian10@gmail.com>
2018-05-14 19:39:09 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,32 @@
+class Course
+
+  attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def get_courses
+
+  end
+
+  def make_courses
+
+  end
+
+  def print_courses
+
+  end
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 
 
+
+end
