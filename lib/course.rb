24c19906222a2e4e92854221fdf04fa235219380
Wasif Khan <waasif.khaan@gmail.com>
2018-05-18 03:15:42 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,20 @@
+class Course 
+  @@all = []
+
+attr_accessor :description , :schedule, :title
+  def initialize #(title,schedule,description)
+    # @title = title
+    # @schedule = schedule
+    # @description = description
+    @@all << self
+  end 
+
+  def self.reset_all
+    self.all.clear
+  end
+  def self.all 
+    @@all 
+  end 
+end
 
 
