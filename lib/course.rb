4d73b2c43b5ad9e2c54da49ca006129f6ecc268f
Chris Alberti <christopherjalberti@gmail.com>
2018-05-30 14:28:49 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  ALL = []
 
+  def initialize
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def self.reset_all
+    ALL.clear
+  end
+
+end
