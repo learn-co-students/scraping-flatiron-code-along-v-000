aa66935f404d4001b55cf72a5f4fae97f5473c5b
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-06 02:11:15 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
+class Course
+    attr_accessor :title, :schedule, :description
 
+    @@allcourses = []
 
+    def initialize
+        @@allcourses << self
+    end
+
+    def self.all
+        @@allcourses
+    end
+
+    def self.reset_all
+        @@allcourses.clear
+    end
+end
