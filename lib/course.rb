58daa5f05f87c8de1f78e50ec3d968a65134c130
Micah Shute <micah.shute@gmail.com>
2018-07-09 01:02:49 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,9 @@
+require_relative 'tracker'
 
+class Course
+  extend Tracker::Class
+  include Tracker::Instance
 
+  attr_accessor :title, :schedule, :description
+
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,47 @@
 require 'nokogiri'
 require 'open-uri'
-
 require_relative './course.rb'
 
+
 class Scraper
-  
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
+
+  attr_accessor :url
+
+  def initialize(url = "http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    @url = url
+
+  end
+
+  def get_page
+    Nokogiri::HTML(open(@url))
+  end
+
+  def get_courses
+    get_page.css("article.post")
+  end
+
+  def make_courses
+    courses_raw = get_courses
+    courses = courses_raw.map do |raw|
+      course = Course.new
+      course.title = raw.css("h2").text
+      course.description = raw.css("p").text
+      course.schedule = raw.css(".date").text
+      course
     end
   end
-  
+
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+    if course.title
+      puts "Title: #{course.title}"
+      puts "  Schedule: #{course.schedule}"
+      puts "  Description: #{course.description}"
+    end
+  end
 end
 
+end
 
-
+Scraper.new.print_courses
