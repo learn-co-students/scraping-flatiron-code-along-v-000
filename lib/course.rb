0a2f54788421cb0cde014284b76e367fe9583a4f
Jonathan Hong <jonhong777@gmail.com>
2018-07-18 15:27:03 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,21 @@
+class Course
+  # class variables
+  @@all = []
 
+  # attribute accessors
+  attr_accessor :title, :schedule, :description
 
+  # instance methods
+  def initialize
+    @@all << self
+  end
+
+  # class methods
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
