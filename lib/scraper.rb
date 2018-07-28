ed71d05938a68f4dcc62519827674e1ee3ec8df7
Reno Sorensen <renojames19@gmail.com>
2018-07-28 23:09:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,6 +19,13 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
     binding.pry
   end
 
