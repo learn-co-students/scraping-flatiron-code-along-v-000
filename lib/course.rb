0c9bdb0660abbd07d58a7a8f1a5587ce6792ab53
Azhar Shah <azharshahstl@yahoo.com>
2018-08-15 21:23:00 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,21 @@
-
+class Course
+  
+  @@all = []
+  
+  attr_accessor :title, :schedule, :description 
+  
+  def initialize 
+    @@all << self
+  end
+  
+  def self.all
+  @@all 
+  end
+  
+  def self.reset_all 
+    self.all.clear 
+  end 
+  
+  
+end
 
