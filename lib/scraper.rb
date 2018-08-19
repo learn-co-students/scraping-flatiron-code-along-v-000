85d64988e3a65bfcbc410db1b49dbdea39baf440
Sharmaine Requerme <srqrme@gmail.com>
2018-08-19 23:26:50 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,4 +12,13 @@ class Scraper
   def get_courses
     self.get_page.css(".post")
   end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
 end
