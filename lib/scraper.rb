59d5dbc23e8b7f5215b7c9e22e7bc3fddf20a6f5
Nick Romans <nicholas.romans@gmail.com>
2018-10-03 00:49:11 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,19 +5,31 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  
-  def get_page
-    
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")) #actual page
-    
-    doc.css(".post").each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
+ 
+    def get_page
+      
+      Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      #doc.css(".post").each do |post|
+        #course = Course.new
+        #course.title = post.css("h2").text
+        #course.schedule = post.css(".date").text
+        #course.description = post.css("p").text
+      #end
     end
-    binding.pry
-  end
+  
+    def get_courses
+      self.get_page.css(".post")
+    end
+
+    def make_courses
+      self.get_courses.each do |post|
+        course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+      end
+    end
   
     def print_courses
     self.make_courses
@@ -35,3 +47,4 @@ end
 #doc.css(".post").first.css("h2").text Course Title
 #doc.css(".post").first.css(".date").text Schedule
 #doc.css(".post").first.css("p").text Description
+Scraper.new.print_courses
