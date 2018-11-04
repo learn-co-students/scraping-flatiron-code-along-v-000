a54b62cef9b3cccccde80379260c39482b15c509
Dana Palmer <roiai4@hotmail.com>
2018-11-04 02:47:23 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,17 @@ class Scraper
     end
   end
 
+def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
 
 end
 
-Scraper.new.get_page
+Scraper.new.print_courses
