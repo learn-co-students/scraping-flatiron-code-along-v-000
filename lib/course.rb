d9d945c86804c2377e3966b5162ca756dfa96a9c
Elias Ventura <eliasventura23@gmail.com>
2018-11-05 19:43:03 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,18 +1,18 @@
 class Course
-
   attr_accessor :title, :schedule, :description
 
-  @@all= []
+  @@all = []
 
   def initialize
-   @@all << self
- end
+    @@all << self
+  end
 
- def self.all
-   @@all
- end
+  def self.all
+    @@all
+  end
 
   def self.reset_all
     @@all.clear
   end
+
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,17 @@
+require_relative './course.rb'
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-require_relative './course.rb'
-
-  #doc.css(".post").each do |post|
-    #course = Course.new
-    #course.title = post.css("h2").text
-    #course.schedule = post.css(".date").text
-    #course.description = post.css("p").text
 class Scraper
+
   def get_page
-  doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    #doc.css(".post").each do |post|
+      #course = Course.new
+      #course.title = post.css("h2").text
+      #course.schedule = post.css(".date").text
+      #course.description = post.css("p").text
   end
 
   def get_courses
@@ -19,25 +19,28 @@ class Scraper
   end
 
   def make_courses
-  self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
-end
 
-def print_courses
-   self.make_courses
-   Course.all.each do |course|
+
+
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
      if course.title
        puts "Title: #{course.title}"
        puts "  Schedule: #{course.schedule}"
        puts "  Description: #{course.description}"
      end
    end
+  end
+
  end
 
-end
-
 Scraper.new.print_courses
+binding.pry
