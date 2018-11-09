f5e099a813f821d15b59086f33144084911a9c8f
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-09 21:05:58 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
 
-
+class Course
+  attr_accessor :title, :schedule, :description
+  
+  @@courses = []
+  
+  def initialize
+   @@courses << self
+  end
+  
+  def self.all
+    @@courses
+  end
+  
+  def self.reset_all
+    @@courses.clear
+  end
+end
