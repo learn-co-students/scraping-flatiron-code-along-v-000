d4725cfb5be504bcf3595e64cc010a11440e97b5
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-11 17:42:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,9 +23,17 @@ class Scraper
   end
   
   def get_courses 
-    
+    get_page.css(".post")
   end 
   
+  def make_courses 
+    get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end 
+  end 
   
 end
 
