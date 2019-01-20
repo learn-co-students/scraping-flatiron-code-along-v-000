9379e31c15a298dd577f2f506a66125fe0ba08db
Steve Albonico <salbonico@gmail.com>
2019-01-20 20:37:04 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+class Course
+@@all = []
+attr_accessor :title, :schedule, :description
+
+def initialize
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def self.reset_all
+  @@all.clear
+end
 
 
+
+end
