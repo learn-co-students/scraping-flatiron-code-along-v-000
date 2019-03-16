7e8697fc3821187f5f211f8026c839b30797ad36
Erica Murdock-Waters <ericahpa@gmail.com>
2019-03-16 07:46:58 +0000

    removed comments/notes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,34 +2,22 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-
 require_relative './course.rb'
-#Require `Course` class file so `Scraper` can make new courses and give them attributes scraped from webpage
 
 class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
-    #doc.css(".post").each do |post|
-      #course = Course.new
-      #course.title = post.css("h2").text
-      #course.schedule = post.css(".date").text
-      #course.description = post.css("p").text
   end
 
-  # get_courses method should operate on the HTML page
-    # which is the return value of the .get_page method
   def get_courses
     self.get_page.css(".post")
-    # return the collection of Nokogiri XML elements that describe each course
   end
 
-  # make_courses method should operate on collection of course offering Nokogiri XML elements returned by #.get_courses method
   def make_courses
-    self.get_courses.each do |post| # iterate over the collection
-      course = Course.new # make a new instance of `Course` class for each one
-      course.title = post.css("h2").text # while assigning it the appropriate attributes
+    self.get_courses.each do |post| 
+      course = Course.new
+      course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
