8992526517beecf0ad4e4ef370f1dc9ca078ff12
Niusha H <niusha.hadziomerovic@gmail.com>
2019-04-05 20:50:10 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,19 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def get_page
+    #using Nokogiri and open-uri to grab the entire HTML document from the web page
+    open-uri
+    
+  end
+
+
+  def get_courses
+
+  end
+  
+  
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
