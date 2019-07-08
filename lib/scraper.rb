5f5d798c12e3c66830d2d7f95064fa831e2c3f68
maxwellbenton <maxwellbenton@gmail.com>
2019-07-08 13:29:32 -0400

    updates starter code

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
     def print_courses
     self.make_courses
     Course.all.each do |course|
-      if course.title
+      if course.title && course.title != ""
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
