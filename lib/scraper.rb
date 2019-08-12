7ca0c76718e617fba637a5754ac2883bde02ca89
Drake Rentrop <drakerentrop@gmail.com>
2019-08-12 18:50:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,6 +16,18 @@ class Scraper
     #binding.pry
   end
 
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
 
     def print_courses
     self.make_courses
