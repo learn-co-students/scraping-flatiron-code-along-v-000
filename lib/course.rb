7ab6e9f0cce13b51066db324cafa5002b64078c5
Luis Herrera <herrera.lrh@gmail.com>
2019-11-11 19:01:32 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -14,5 +14,4 @@ class Course
   def self.reset_all
     @@all.clear
   end
-  
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,8 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def get_courses
@@ -18,7 +17,7 @@ class Scraper
     self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
-      course.schedule = post.css("date").text
+      course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
   end
@@ -33,6 +32,7 @@ class Scraper
       end
     end
   end
+
 end
 
 Scraper.new.print_courses
