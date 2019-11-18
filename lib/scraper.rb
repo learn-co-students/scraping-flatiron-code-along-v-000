161c0dc8f48552826884b651226a3605d7aafb58
Kimberly Aviles <01010110.v@gmail.com>
2019-11-18 20:57:26 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,7 @@
 require 'nokogiri' # Ruby Gem; turn giant HTML string into nested objects
 require 'open-uri' # Ruby module; allows HTTP requests
 
-require 'pry'
-
-require_relative './course.rb'
+require_relative './course.rb' # use the COURSEclass template
 
 class Scraper #parse a web page's HTML & pull pertinent data
   # Use Nokogiri
@@ -11,11 +9,22 @@ class Scraper #parse a web page's HTML & pull pertinent data
   # Use scraped DATA to INSTANTIATE instances of COURSEclass
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    #OPEN is an OPEN-URI method
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")) #OPEN is an OPEN-URI method
+    #grab the URL's HTML using OPEN-URI, access the HTMLclassmethods of NOKOGIRI and store the nested node translation in DOCvariable <<< WRITE BETTER PLZ
 
-    binding.pry
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css(".p").text
+    # end
 
+  end
+
+  def get_courses
+  end
+
+  def make_courses
   end
 
 
@@ -25,5 +34,3 @@ class Scraper #parse a web page's HTML & pull pertinent data
 
 
 end #<----CLASS END
-
-Scraper.new.get_page
