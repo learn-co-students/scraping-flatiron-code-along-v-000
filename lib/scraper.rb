923ad925b22694631fb2201f661af95ba5df220f
Erica Murdock-Waters <ericahpa@gmail.com>
2020-03-14 18:47:11 +0000

    removed unnecessary doc variable from Scraper.get_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def get_courses
@@ -15,7 +15,7 @@ class Scraper
   end
 
   def make_courses
-    self.get_courses.each do |post| 
+    self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
@@ -26,7 +26,7 @@ class Scraper
   def print_courses
     self.make_courses
     Course.all.each do |course|
-      if course.title
+      if course.title && course.title != ""
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
