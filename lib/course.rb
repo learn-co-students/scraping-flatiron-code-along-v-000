02dd2446c28a2d6381a0ab8ae473f0288c4a2600
Lamia Haider <lamia.haider@gmail.com>
2020-06-03 21:52:52 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,15 +1,18 @@
 class Course
   attr_accessor :title, :schedule, :description
+
   @@all = []
 
   def initialize
-  @@all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
+
   def self.reset_all
     @@all.clear
   end
+
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,43 +1,27 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-require_relative './course.rb'
+require "nokogiri"
+require "pry"
+require "open-uri"
+
+require_relative "./course.rb"
 
 class Scraper
+
   def get_page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    #doc.css(".post").each do |post|
-    #  course = Course.new
-    #  course.title = post.css("h2").text
-    #  course.schedule = post.css(".date").text
-    #  course.description = post.css("p").text
-    #  binding.pry
-    #end
+
   end
+
   def get_courses
     self.get_page.css(".post")
   end
 
   def make_courses
-    self.get_courses.each do |post|
+    self.get_courses.each do |cl|
       course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
+      course.title = cl.css("h2").text
+      course.schedule = cl.css(".date").text
+      course.description = cl.css("p").text
     end
   end
 
-   def print_courses
-     self.make_courses
-     Course.all.each do |course|
-       if course.title
-         puts "Title: #{course.title}"
-         puts "  Schedule: #{course.schedule}"
-         puts "  Description: #{course.description}"
-       end
-     end
-   end
- end
-
-Scraper.new.get_page
-Scraper.new.print_courses
+end
