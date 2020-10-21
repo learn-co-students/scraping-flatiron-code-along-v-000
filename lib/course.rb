0c17852bf39890a1cf4818b5a6689b2a6c863086
Jaime Johnsen <drivetraindiva@gmail.com>
2020-10-21 03:14:17 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -2,10 +2,18 @@ class Course
 
   attr_accessor :title, :schedule, :description
 
+  @@all = []
+
   def initialize
-    @title = title
-    @schedule = schedule
-    @description = description
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
   end
 
 end
