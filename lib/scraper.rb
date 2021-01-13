d6365791cb136efc6ba6bc405ffa7ab8cc51f187
ritchey0713 <ritchey0713@gmail.com>
2021-01-13 11:22:03 -0500

    commit tree b0c68e49c842d57735dc72629c49f8da3e5ff60e

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,24 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +32,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
